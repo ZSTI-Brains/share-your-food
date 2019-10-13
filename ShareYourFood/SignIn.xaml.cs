b7ca725acce876182e24392a834c9bef8f6c09b8
Marcin <wojtasmarcin.20@gmail.com>
2019-10-13 15:23:58 +0200

    Change SignIN.xaml.cs

--- ShareYourFood/SignIn.xaml.cs
+++ ShareYourFood/SignIn.xaml.cs
@@ -39,28 +39,4 @@ namespace ShareYourFood
         }
 
     }
-    /*public class HttpClient : HttpResponseMessage
-    {
-        static readonly HttpClient client = new HttpClient();
-
-        static async Task Connect()
-        {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
-            try
-            {
-                using (HttpResponseMessage response = await client.GetAsync("http://www.szaredko.com/share-your-food"))
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-
-                    Console.WriteLine(responseBody);
-                }
-                    
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("Message :{0} ", e.Message);
-            }
-        }
-    }*/
 }
