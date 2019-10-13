9d8bcd4199ef7ec76d130813e81f3a118cd5b316
Marcin <wojtasmarcin.20@gmail.com>
2019-10-13 21:01:57 +0200

    Update

--- ShareYourFood/SignIn.xaml.cs
+++ ShareYourFood/SignIn.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,18 +21,26 @@ namespace ShareYourFood
             InitializeComponent();
             Title = "Logowanie";
 
-            GetRequest("http://www.szaredko.com/share-your-food");
+            PostRequest("http://www.szaredko.com/share-your-food");
         }
 
-        async static void GetRequest(string url)
+        async static void PostRequest(string url)
         {
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("email","marcin.gortat@nba.com"),
+                new KeyValuePair<string, string>("password","nbaimissya")
+            };
+            HttpContent q = new FormUrlEncodedContent(queries);
             using (HttpClient client = new HttpClient())
             {
-                using (HttpResponseMessage response = await client.GetAsync(url))
+                using (HttpResponseMessage response = await client.PostAsync(url, q))
                 {
                     using (HttpContent content = response.Content)
                     {
                         string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+
                         Console.WriteLine(mycontent);
                     }
                 }
