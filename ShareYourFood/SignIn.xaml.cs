a101aa3f9d2c48d6d499856b2972f2d57b2690be
Marcin <wojtasmarcin.20@gmail.com>
2019-10-13 15:23:18 +0200

    Change SignIN.xaml.cs

--- ShareYourFood/SignIn.xaml.cs
+++ ShareYourFood/SignIn.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +19,48 @@ namespace ShareYourFood
         {
             InitializeComponent();
             Title = "Logowanie";
+
+            GetRequest("http://www.szaredko.com/share-your-food");
         }
+
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+
     }
+    /*public class HttpClient : HttpResponseMessage
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Connect()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                using (HttpResponseMessage response = await client.GetAsync("http://www.szaredko.com/share-your-food"))
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(responseBody);
+                }
+                    
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }*/
 }
