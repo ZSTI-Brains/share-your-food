ef0dd63b3f5bdc9eb563503dead8ef866ed836eb
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-10-14 20:27:05 +0200

    Added Xamarin.Forms.Maps

--- ShareYourFood/MapPage.xaml.cs
+++ ShareYourFood/MapPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace ShareYourFood
 {
@@ -15,6 +16,18 @@ namespace ShareYourFood
         public MapPage()
         {
             InitializeComponent();
+            var map = new Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
         }
     }
 }
