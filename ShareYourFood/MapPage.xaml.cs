4b2fddfc1305142393f012f283f265a17c6ad4fe
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-10-15 07:35:30 +0200

    Updated map

--- ShareYourFood/MapPage.xaml.cs
+++ ShareYourFood/MapPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Maps;
+using Xamarin.Essentials;
 
 namespace ShareYourFood
 {
@@ -16,9 +17,28 @@ namespace ShareYourFood
         public MapPage()
         {
             InitializeComponent();
-            var map = new Map(
-            MapSpan.FromCenterAndRadius(
-                    new Position(37, -122), Distance.FromMiles(0.3)))
+            var position = new Position(49.673971, 20.079831);
+
+            var map = new Xamarin.Forms.Maps.Map(
+                MapSpan.FromCenterAndRadius(position, Distance.FromMiles(0.5f)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
+
+        async void InitializeAsync()
+        {
+            var location = await Geolocation.GetLocationAsync();
+            var position = new Position(49.673971, 20.079831);
+
+            var map = new Xamarin.Forms.Maps.Map(
+                MapSpan.FromCenterAndRadius(position, Distance.FromMiles(0.5f)))
             {
                 IsShowingUser = true,
                 HeightRequest = 100,
