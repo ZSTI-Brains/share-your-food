ea664466d76f1ebb92e018b346b62aab6a18d43f
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-10-18 07:18:28 +0200

    Added Web.cs

--- PHP/get-eating-houses-info.php
+++ PHP/get-eating-houses-info.php
@@ -2,18 +2,18 @@
     require_once("db-connection.php");
 
     $mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
-    $mysqli->set_charset("uft8");
+    $mysqli->set_charset("utf8");
 
     $query = "SELECT eh.name, eh.street_name, eh.house_number, eh.apartament_number, eah.latitude, eah.longitude
               FROM eating_houses AS eh JOIN eating_house_coordinates AS eah ON eh.house_id = eah.house_id";
     $result = $mysqli->query($query);
 
-    $eh = [];
+    $arr = array();
     while ($row = $result->fetch_assoc()) {
-        array_push($eh, $row);
+        $arr[] = $row;
     }
 
-    echo json_encode($eh[0]);
+    echo json_encode($arr, JSON_UNESCAPED_UNICODE);
 
     $mysqli->close();
 ?>
---
+++ ShareYourFood/Web.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ShareYourFood
+{
+    class Web
+    {
+        private static HttpClient client = new HttpClient();
+
+        public async static string Post(string url, Dictionary<string, string> v = null)
+        {
+            var content = new FormUrlEncodedContent(v);
+            var response = await client.PostAsync(url, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }
+    }
+}
